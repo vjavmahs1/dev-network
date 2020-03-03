c07c30d7b43da95e8a8731e91b632136e1e19d83
kimsj1993 <kimsj1993@gmail.com>
2020-03-03 01:34:23 -0600

    implemented create and update profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile')
 const { check, validationResult} = require('express-validator');
+const normalize = require('normalize-url');
+
 
 
 // @route GET api/profile/me
@@ -40,7 +42,54 @@ router.post('/', [auth, [
         return res.status(400).json({errors : errors.array()});
     }
 
-    
+    const {
+        company,
+        location,
+        website,
+        bio,
+        skills,
+        status,
+        githubusername,
+        twitter,
+        instagram,
+        linkedin
+      } = req.body;
+
+
+      const profileFields = {
+        user: req.user.id,
+        company,
+        location,
+        website: website === '' ? '' : normalize(website, { forceHttps: true }),
+        bio,
+        skills: Array.isArray(skills)
+          ? skills
+          : skills.split(',').map(skill => ' ' + skill.trim()),
+        status,
+        githubusername
+      };
+
+          // Build social object and add to profileFields
+    const socialfields = { twitter, instagram, linkedin };
+
+    for (const [key, value] of Object.entries(socialfields)) {
+      if (value)
+        socialfields[key] = normalize(value, { forceHttps: true });
+    }
+    profileFields.social = socialfields;
+
+    try {
+        // Using upsert option (creates new doc if no match is found):
+        let profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true, upsert: true }
+        );
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
 
 })
 
